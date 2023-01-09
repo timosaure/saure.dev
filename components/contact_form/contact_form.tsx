42ec1f52db3aee26f82419fcc3e50aa7a2823657
Timo Saure <sauretimo@googlemail.com>
2023-01-09 23:38:18 +0100

    Enable contact form for netlify

--- components/contact_form/contact_form.tsx
+++ components/contact_form/contact_form.tsx
@@ -1,6 +1,7 @@
 
 import { IconDefinition, faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FormEvent, useState } from "react";
 
 export default function ContactSection({ id }: { id: string }) {
   return (
@@ -17,10 +18,32 @@ export default function ContactSection({ id }: { id: string }) {
 
 function ContactForm(props: { name: string; successPath: string; }) {
 
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const data = new URLSearchParams()
+    data.append("form-name", "contact")
+    data.append("name", name)
+    data.append("mail", mail)
+    data.append("message", message)
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: data,
+    })
+      .then(() => alert("Thank you for your submission"))
+      .catch((error) => alert(error));
+  };
+
   return (
-    <form className="text-gray-700">
-      <FormInput name="name" label="Name" type="text" icon={faUser} />
-      <FormInput name="mail" label="Email" type="email" icon={faEnvelope} />
+    <form className="text-gray-700" name="contact" data-netlify="true" onSubmit={handleSubmit}>
+      <FormInput name="name" label="Name" type="text" icon={faUser} value={name} onChange={setName} />
+      <FormInput name="mail" label="Email" type="email" icon={faEnvelope} value={mail} onChange={setMail} />
       <div className="mb-3">
         <label className="font-bold block mt-3 mb-2" htmlFor="message-input">
           Message
@@ -31,6 +54,8 @@ function ContactForm(props: { name: string; successPath: string; }) {
             name="message"
             className="p-2 min-h-[12em] border border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-lg w-full"
             placeholder="Textarea"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
           ></textarea>
         </div>
       </div>
@@ -43,8 +68,7 @@ function ContactForm(props: { name: string; successPath: string; }) {
   );
 }
 
-function FormInput(props: { name: string; type: string; label: string; icon: IconDefinition; }) {
-  const { name, type, label, icon } = props;
+function FormInput({ name, type, label, icon, value, onChange }: { name: string, type: string, label: string, icon: IconDefinition, value: string, onChange: (s: string) => void }) {
   const id = name + "-input";
   return (
     <div className="">
@@ -57,7 +81,9 @@ function FormInput(props: { name: string; type: string; label: string; icon: Ico
           id={id}
           type={type}
           className="pl-10 p-2 border border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-lg w-full"
-          placeholder={label} />
+          placeholder={label}
+          value={value}
+          onChange={(e) => onChange(e.target.value)} />
         <span className="pointer-events-none absolute top-0 left-0 z-10 text-gray-400 flex justify-center items-center h-full px-4">
           <FontAwesomeIcon icon={icon} />
         </span>
