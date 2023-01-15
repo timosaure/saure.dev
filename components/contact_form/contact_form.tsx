ee9d35e7c6d6721742d68f0a8fbd781c3d25fb52
Timo Saure <sauretimo@googlemail.com>
2023-01-15 12:15:43 +0100

    Add success and fail message for contact form

--- components/contact_form/contact_form.tsx
+++ components/contact_form/contact_form.tsx
@@ -16,8 +16,33 @@ export default function ContactSection({ id }: { id: string }) {
   );
 }
 
+function SuccessMessage() {
+  return (
+    <div className="bg-primary text-white border border-solid border-green-900 rounded-2xl flex flex-col justify-center items-center h-24">
+      <h3 className="font-bold text-lg">Ihre Nachricht wurde erfolgreich verschickt!</h3>
+      <p className="">Ich werde mich sobald wie möglich zurückmelden.</p>
+    </div>
+  )
+}
+
+function FailureMessage() {
+  return (
+    <div className="bg-red-600 text-white border border-solid border-red-600 rounded-2xl flex flex-col justify-center items-center h-24">
+      <h3 className="font-bold text-lg">Ihre Nachricht konnte nicht verschickt werden!</h3>
+      <p className="">Bitte senden sie mir stattdessen eine Mail an <a className="text-blue-800 hover:underline" href="mailto:hallo@saure.dev">hallo@saure.dev</a>.</p>
+    </div>
+  )
+}
+
+enum SendStatus {
+  NOT_SENT,
+  SUCCESS,
+  FAIL
+}
+
 function ContactForm(props: { name: string; successPath: string; }) {
 
+  const [success, setSuccess] = useState(SendStatus.NOT_SENT);
   const [name, setName] = useState("");
   const [mail, setMail] = useState("");
   const [message, setMessage] = useState("");
@@ -36,36 +61,53 @@ function ContactForm(props: { name: string; successPath: string; }) {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: data,
     })
-      .then(() => alert("Thank you for your submission"))
-      .catch((error) => alert(error));
+      .then((r) => {
+        if (r.ok) {
+          setSuccess(SendStatus.SUCCESS);
+        }
+        throw new Error("Failed to send message!" + r.status)
+      })
+      .catch((error) => {
+        console.log(error)
+        setSuccess(SendStatus.FAIL);
+      });
   };
 
-  return (
-    <form className="text-gray-700" name="contact" data-netlify="true" onSubmit={handleSubmit}>
-      <FormInput name="name" label="Name" type="text" icon={faUser} value={name} onChange={setName} />
-      <FormInput name="mail" label="Email" type="email" icon={faEnvelope} value={mail} onChange={setMail} />
-      <div className="mb-3">
-        <label className="font-bold block mt-3 mb-2" htmlFor="message-input">
-          Message
-        </label>
-        <div className="control">
-          <textarea
-            id="message-input"
-            name="message"
-            className="p-2 min-h-[12em] border border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-lg w-full"
-            placeholder="Textarea"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-          ></textarea>
-        </div>
-      </div>
-      <div>
-        <button type="submit" className="bg-primary rounded-md text-white py-2 px-4">
-          Senden
-        </button>
-      </div>
-    </form>
-  );
+  switch (success) {
+    case SendStatus.NOT_SENT:
+    case SendStatus.FAIL:
+      return (
+        <form className="text-gray-700" name="contact" data-netlify="true" onSubmit={handleSubmit}>
+          {success === SendStatus.FAIL ? <FailureMessage /> : <></>}
+          <FormInput name="name" label="Name" type="text" icon={faUser} value={name} onChange={setName} />
+          <FormInput name="mail" label="Email" type="email" icon={faEnvelope} value={mail} onChange={setMail} />
+          <div className="mb-3">
+            <label className="font-bold block mt-3 mb-2" htmlFor="message-input">
+              Message
+            </label>
+            <div className="control">
+              <textarea
+                id="message-input"
+                name="message"
+                className="p-2 min-h-[12em] border border-gray-300 focus:border-blue-500 focus:ring-blue-500 rounded-lg w-full"
+                placeholder="Textarea"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+              ></textarea>
+            </div>
+          </div>
+          <div>
+            <button type="submit" className="bg-primary rounded-md text-white py-2 px-4">
+              Senden
+            </button>
+          </div>
+        </form>
+      );
+    case SendStatus.SUCCESS:
+      return (<SuccessMessage />)
+  }
+
+
 }
 
 function FormInput({ name, type, label, icon, value, onChange }: { name: string, type: string, label: string, icon: IconDefinition, value: string, onChange: (s: string) => void }) {
